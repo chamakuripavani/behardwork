10e8dda0c974827acb6fd4171ad3e92f4f8a2727
chamakuripavani <158549727+chamakuripavani@users.noreply.github.com>
2024-07-04 20:58:52 +0530

    Update HelpfulPerson.java

--- src/KalyanSir/HelpfulPerson.java
+++ src/KalyanSir/HelpfulPerson.java
@@ -8,6 +8,12 @@ public class HelpfulPerson
 		System.out.println("our main agenda is to teach core"
 				+ " functionality to students like as primary colours");
 		System.out.println("Arif is a good kind hearted person");
+		System.out.println("Arif is a good person");
+	 System.out.println("Arif is a helpful  person");
+
+
+
+		
 	}
 
 }
